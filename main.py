1c217d8ecbc185af5976e53d732c526f4ff1bdf0
TheLostSamoerai <simonbuyck@icloud.com>
2023-12-07 21:21:25 +0100

    hh

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from window import MainWindow
+from editor import Editor
 
 icon = None
 
-window = MainWindow(2460, 1440, 144, "SamoeraiEngineV0.1", None, None)
+window = MainWindow(2460, 1440, 144, "SamoeraiEngineV0.1", None, Editor)
 MainWindow.run()
 
 
